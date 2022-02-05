254e8ceffc408c77b184fa0b9dbea7a4e29c460f
shehriyar <shehriyarnadeemy@gmail.comm>
2022-02-06 00:27:27 +0500

    Push

--- pages/blogs/[slug].tsx
+++ pages/blogs/[slug].tsx
@@ -2,16 +2,34 @@ import React from 'react'
 import { sendGraphQlQueryPost } from '../../common/utils/apiUtils';
 import { getBlogPost, getUserArticles } from '../../common/GraphQlQueries';
 import { BlogContent, BlogDetails } from '../../components/blog';
+import Head from 'next/head';
 
 function BlogPost(props) {
     const { data: { post } } = props.data;
-    return (   
+    return (
+        <>
+        <Head>
+        <title>{post.title}</title>
+        <meta
+        name="description"
+        content= {post.brief} />
+        <meta
+        name="keywords"
+        content="Javascript, Building Websites, Learn React, Software Engineer Blogs, Learn Javascript"
+        />
+        <meta name="image" content={post.coverImage} />
+        <meta property="og:url" content="http://shehriyarnadeem.dev" />
+        <meta property="og:title" content={post.title} />
+        <meta property="og:description"content={post.brief} />
+        <meta property="og:image" content={post.coverImage} />
+    </Head>    
         <div className="bg-primary dark:bg-primary">
             <article className="mx-auto p-5 max-w-5xl">
               <BlogDetails post={post} />
               <BlogContent  content={post.contentMarkdown} />
             </article>
         </div>
+        </>
     )
 }
 
