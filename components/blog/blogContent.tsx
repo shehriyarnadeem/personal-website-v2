b22c682b6a2eca677bfada621c7a25f9c12a409f
shehriyar <shehriyar.nadeem@elevate-digital.com>
2022-10-28 13:32:53 +0500

    Fixed IFrame embedd issue

--- components/blog/blogContent.tsx
+++ components/blog/blogContent.tsx
@@ -3,11 +3,14 @@ import ReactMarkdown from "react-markdown";
 import CodeBlock from '../CodeBlock';
 import remarkGfm from 'remark-gfm'
 import mardownCss from '../../styles/markdown.module.css'
+import rehypeRaw from 'rehype-raw'
+
 function BlogContent({content}) {
   return (
   <div>
       <ReactMarkdown 
        className={mardownCss['reactMarkDown']} 
+       rehypePlugins={[rehypeRaw]}
        components={CodeBlock}
        remarkPlugins={[remarkGfm]}
       >
